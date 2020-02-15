e06b7f99126e5da0f8dccf68fd1da383b91936c5
vinmugambi <vinmugambi17@gmail.com>
2020-02-15 23:49:24 +0300

    create database relations

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['title', 'body', 'user_id'];
+
+    public function author()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function post_images()
+    {
+        return $this->hasMany('App\PostImage');
+    }
 }
--- app/PostImage.php
+++ app/PostImage.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostImage extends Model
 {
-    //
+    protected $fillable = ['post_id', 'post_image_path', 'post_image_caption'];
+
+    public function post()
+    {
+        $this->belongsTo('App\Post');
+    }
 }
