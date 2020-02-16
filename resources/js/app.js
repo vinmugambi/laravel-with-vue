f335a3a54397d064b09f28d0185adee821bc20be
vinmugambi <vinmugambi17@gmail.com>
2020-02-16 23:46:26 +0300

    add element ui and vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,12 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import store from './store/index';
+import ElementUI from  'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -20,6 +26,8 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('create-post', require('./components/CreatePost.vue').default);
+Vue.component('all-posts', require('./components/AllPosts.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +37,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    store
 });
