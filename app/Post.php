a01efbe989aaecce6dde6654761c9c18e1a4abe2
vinmugambi <vinmugambi17@gmail.com>
2020-02-21 23:12:29 +0300

    add markdown parser

--- app/Post.php
+++ app/Post.php
@@ -2,11 +2,17 @@
 
 namespace App;
 
+use Faker\Provider\HtmlLorem;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\HtmlString;
 
 class Post extends Model
 {
     protected $fillable = ['title', 'body', 'user_id'];
+
+    public static function getTextAttribute($value){
+        return new HtmlString(app(Parsedown::class)->setSafeMode(true)->text($value));
+    }
 
     public function author()
     {
